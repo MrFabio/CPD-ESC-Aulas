c463c8c153ae8fe3d78856838a7d46d4518b51ef
Fabio Gomes mrfabiogomes@gmail.com <mrfabiogmes@gmail.com>
2015-05-08 03:05:29 +0100

    Beta gnuplot Scripts

--- 2-io/xls/gnuplot_script.sh
+++ 2-io/xls/gnuplot_script.sh
@@ -1,16 +1,13 @@
-
-for dir in */; do
-	cd dir
-	gnuplot < "
-	set term png
-	set output '$dir.png'
-	set logscale x 2
-	set logscale y 2
-	set autoscale z
-	set grid lt 2 lw 1
-	set zlabel 'Kbytes/sec'
-	set style data lines
-	set dgrid3d 80,80,3
-	splot '$dir.gnuplot' title '$dir'
-	quit"
-done
+#for dir in */; do
+#	cd $dir
+	for file in *.txt; do
+		gengnuplot.sh $file write
+		cd write
+		cp $file.gnuplot file.gnuplot
+		gnuplot < ../gp.txt
+		mv file.png $file.png
+		rm file.gnuplot
+		cd ..
+	done
+#	cd ..
+#done
